e4d0afc7ff1c1d27dd6c9b49b5f938681be6c1d1
Todd B. Adams <toddbadams@msn.com>
2015-01-08 15:35:05 +0000

    js only implementation

--- app.js
+++ app.js
@@ -1,44 +1,79 @@
-﻿(function(angular) {
+﻿(function() {
     'use strict';
 
-    var _moduleId = 'abd',
-        _start = new Date().getTime();
+    // Uses only pure JS, no external libraries
+    var _start = new Date().getTime(),
+        _data = null;
 
-    angular.module(_moduleId, [])
-        .directive("onLastRepeat", onLastRepeat)
-        .controller('bigdata', controller);
+    activate();
 
-    controller.$inject = ['$scope', '$http'];
 
-    function controller($scope, $http) {
-        var _vm = this;
-
-        log('controller started');
+    function activate() {
+        log('activation started');
 
         // load the data
-        $http.get('data.json')
-            .success(onDataLoad);
+        loadJSON('data.json', onDataLoad);
 
-        $scope.$on('onRepeatLast', function(scope, element, attrs) {
-            log('rows loaded');
-        });
+    }
 
+    function onDataLoad(response) {
+        // Parse JSON string into object
+        _data = JSON.parse(response);
+        log('data loaded');
+        createTable(_data);
+    }
 
-        function onDataLoad(data) {
-            log('data loaded');
-            $scope.data = data;
+    function createTable(data) {
+        var _rowData = data.rows,
+            _i,
+            _l = _rowData.length,
+            _table = document.createElement('table'),
+            _tbody = document.createElement('tbody'),
+            _body = document.body;
+
+        for (_i = 0; _i < _l; _i += 1) {
+            _tbody.appendChild(createRow(_rowData[_i]));
         }
+        _table.appendChild(_tbody);
+        _body.appendChild(_table);
+        log('rows added')
+    }
+
+    function createRow(data) {
+        var _cellData = data.values,
+            _i,
+            _l = _cellData.length,
+            _tr = document.createElement('tr');
+
+        for (_i = 0; _i < _l; _i += 1) {
+            _tr.appendChild(createCell(_cellData[_i]));
+        }
+        return _tr;
+    }
+
+    function createCell(data) {
+        var _td = document.createElement('td'),
+            _tdContent = document.createTextNode(data.value);
+
+        _td.appendChild(_tdContent);
+        return _td;
     }
 
     function log(message) {
         console.log(message + ' time:' + (new Date().getTime() - _start) + 'ms');
     }
 
-    function onLastRepeat() {
-        return function(scope, element, attrs) {
-            if (scope.$last) setTimeout(function() {
-                scope.$emit('onRepeatLast', element, attrs);
-            }, 1);
+    function loadJSON(filename, callback) {
+        var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+        xobj.open('GET', filename, true); // Replace 'my_data' with the path to your file
+        xobj.onreadystatechange = function() {
+            if (xobj.readyState == 4 && xobj.status == "200") {
+                // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+                callback(xobj.responseText);
+            }
         };
+        xobj.send(null);
     }
-})(angular);
+
+})();
